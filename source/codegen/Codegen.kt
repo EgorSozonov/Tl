d1303d6df3e22f0eaf847c2f8c3fd4f7e2a18e3b
zrx <zrx@zrxcomp.sozonov.tech>
2022-12-17 14:00:47 -0500

    .

--- source/codegen/Codegen.kt
+++ source/codegen/Codegen.kt
@@ -1,9 +1,9 @@
 package codegen
 import lexer.FileType
-import lexer.RegularToken
 import parser.FrameAST
 import parser.Parser
 import parser.FrameAST.*;
+import parser.errorInconsistentExtent
 
 class Codegen(val pr: Parser) {
 
@@ -30,50 +30,170 @@ private fun codeGenExecutable(wr: StringBuilder) {
     val entryPoint = ast.getFunc(pr.indFirstFunction - 1)
     ast.seek(entryPoint.bodyId)
     val entryPointLenNodes = ast.currChunk.nodes[ast.currInd + 3]
-
     backtrack.add(CodegenFrame(functionDef, -1, 0, entryPoint.bodyId, entryPointLenNodes))
-    while (backtrack.isNotEmpty()) {
-        val f = backtrack.last()
-        if (f.currNode == f.sentinelNode) {
-            backtrack.removeLast()
-            closeFrame(f, wr)
-        } else {
-            writeFrame(f, wr)
+
+    while (ast.currTokInd != entryPoint.bodyId + entryPointLenNodes) {
+        val extentType = ast.currNodeType()
+        val lenNodes = ast.currChunk.nodes[ast.currInd + 3]
+        val fr = backtrack.last()
+        if (extentType == functionDef.internalVal.toInt()) {
+            writeFnSignature(fr, lenNodes, wr)
+        } else if (extentType == scope.internalVal.toInt()) {
+            writeScope(fr, lenNodes, wr)
+        } else if (extentType == expression.internalVal.toInt()) {
+            writeExpression(fr, lenNodes, wr)
+        } else if (extentType == statementAssignment.internalVal.toInt()) {
+            writeAssignment(fr, lenNodes, wr)
+        } else if (extentType == fnDefPlaceholder.internalVal.toInt()) {
+            moveIntoFunctionDefinition()
+        } else if (extentType == returnExpression.internalVal.toInt()) {
+            writeReturn(fr, lenNodes, wr)
         }
+        maybeCloseFrames(wr)
+    }
+
+}
+
+private fun pushNewFrame() {
+    val eType = FrameAST.values().first {it.internalVal.toInt() == ast.currChunk.nodes[ast.currInd] ushr 27 }
+    val lenNodes = ast.currChunk.nodes[ast.currInd + 3]
+    var nameId = -1
+    var arity = -1
+    if (eType == fnDefPlaceholder) {
+        val functionSignature = ast.getFunc(ast.currChunk.nodes[ast.currInd + 2])
+        nameId = functionSignature.nameId
+        arity = functionSignature.arity
+    }
+    backtrack.add(CodegenFrame(eType, nameId, arity, ast.currTokInd, lenNodes))
+}
+
+
+private fun moveIntoFunctionDefinition() {
+    val newTokenId = ast.currChunk.nodes[ast.currInd + 2]
+    ast.seek(newTokenId)
+    pushNewFrame()
+}
+
+
+
+
+/**
+ * When we are at the end of a function parsing a parse frame, we might be at the end of said frame
+ * (if we are not => we've encountered a nested frame, like in "1 + { x = 2; x + 1}"),
+ * in which case this function handles all the corresponding stack poppin'.
+ * It also always handles updating all inner frames with consumed tokens.
+ */
+private fun maybeCloseFrames(wr: StringBuilder) {
+    if (backtrack.isEmpty()) return
+
+    val topFrame = backtrack.last()
+    if (topFrame.currNode < topFrame.sentinelNode) return
+
+    var nodesToAdd = topFrame.lenNodes
+    var isFunClose = false
+
+    if (topFrame.eType == functionDef) {
+        isFunClose = true
+    }
+    closeFrame(topFrame, wr)
+    backtrack.removeLast()
+
+    var i = backtrack.size - 1
+    while (i > -1) {
+        val frame = backtrack[i]
+        frame.currNode += nodesToAdd
+        if (frame.currNode < frame.sentinelNode) {
+            break
+        } else {
+            if (frame.eType == FrameAST.functionDef) {
+                isFunClose = true
+            }
+            closeFrame(topFrame, wr)
+            backtrack.removeLast()
+            nodesToAdd = frame.lenNodes + 1
+        }
+        i--
+    }
+
+    if (isFunClose && backtrack.isNotEmpty()) {
+        ast.seek(backtrack.last().currNode + 1) // skipping to the node after the fnDefPlaceholder
     }
 }
 
 private fun writeFrame(fr: CodegenFrame, wr: StringBuilder) {
+    val lenNodes = ast.currChunk.nodes[ast.currInd + 3]
     if (fr.eType == functionDef) {
-        writeSignatureAndBody(fr, wr)
+        writeFnSignature(fr, lenNodes, wr)
     } else if (fr.eType == scope) {
-        indentDepth += 4
-        writeScope(fr, wr)
+
+        writeScope(fr, lenNodes, wr)
     } else if (fr.eType == expression) {
-        writeExpression(fr, wr)
+        writeExpression(fr, lenNodes, wr)
     } else if (fr.eType == statementAssignment) {
-        writeAssignment(fr, wr)
+        writeAssignment(fr, lenNodes, wr)
     } else if (fr.eType == returnExpression) {
-        writeReturn(fr, wr)
+        writeReturn(fr, lenNodes, wr)
     }
 }
 
-private fun writeExpression(fr: CodegenFrame, wr: StringBuilder) {
+
+private fun writeFnSignature(fr: CodegenFrame, lenNodes: Int, wr: StringBuilder) {
+    pushNewFrame()
+
+    val indentation = " ".repeat(indentDepth*4)
+    val funcName = if (fr.nameId > -1) { ast.identifiers[fr.nameId] } else { entryPointName }
+    wr.append(indentation)
+    wr.append("function ")
+    wr.append(funcName)
+    wr.append("(")
+
+
+    ast.seek(fr.currNode)
+    ast.nextNode() // Skipping the "functionDef" node
+    var consumedTokens = 1
+    for (i in 0 until fr.arity) {
+        wr.append(ast.getString(ast.currChunk.nodes[ast.currInd + 2]))
+        if (i == fr.arity - 1) {
+            wr.append(")\n")
+        } else {
+            wr.append(", ")
+        }
+        ast.nextNode()
+        consumedTokens++
+    }
+
+    backtrack.last().currNode += consumedTokens
+
+
+}
+
+
+private fun writeExpression(fr: CodegenFrame, lenNodes: Int, wr: StringBuilder) {
     wr.append("expression")
+    fr.currNode += lenNodes
 }
 
-private fun writeAssignment(fr: CodegenFrame, wr: StringBuilder) {
+    
+private fun writeAssignment(fr: CodegenFrame, lenNodes: Int, wr: StringBuilder) {
     wr.append("assignment")
+
+    fr.currNode += lenNodes
 }
 
-private fun writeReturn(fr: CodegenFrame, wr: StringBuilder) {
+
+private fun writeReturn(fr: CodegenFrame, lenNodes: Int, wr: StringBuilder) {
     wr.append("return")
+    fr.currNode += lenNodes
 }
 
-private fun writeScope(fr: CodegenFrame, wr: StringBuilder) {
+
+private fun writeScope(fr: CodegenFrame, lenNodes: Int, wr: StringBuilder) {
+    pushNewFrame()
+
+    indentDepth += 4
     ast.nextNode()
-    backtrack.last().currNode++
 }
+
 
 private fun closeFrame(fr: CodegenFrame, wr: StringBuilder) {
     if (fr.eType == functionDef) {
@@ -101,55 +221,6 @@ private fun codeGenTests(wr: StringBuilder) {
 }
 
 
-private fun writeSignatureAndBody(fr: CodegenFrame, wr: StringBuilder) {
-    val indentation = " ".repeat(indentDepth*4)
-    val funcName = if (fr.nameId > -1) { ast.identifiers[fr.nameId] } else { entryPointName }
-    wr.append(indentation)
-    wr.append("function ")
-    wr.append(funcName)
-    wr.append("(")
-
-
-    ast.seek(fr.currNode)
-    ast.nextNode() // Skipping the "functionDef" node
-    var consumedTokens = 1
-    for (i in 0 until fr.arity) {
-        wr.append(ast.getString(ast.currChunk.nodes[ast.currInd + 2]))
-        if (i == fr.arity - 1) {
-            wr.append(")\n")
-        } else {
-            wr.append(", ")
-        }
-        ast.nextNode()
-        consumedTokens++
-    }
-
-    backtrack.last().currNode += consumedTokens
-    val nextFrameType = FrameAST.values().firstOrNull { it.internalVal == ast.currNodeType().toByte() }
-    this.backtrack.add(CodegenFrame(nextFrameType!!, -1, -1))
-}
-
-/**
- * Builds the tree of function definitions with data on their bodies and child definitions
- */
-//private fun buildFunctionTree(): ArrayList<CodegenFunc> {
-//    val baseId = pr.indFirstFunction - 1
-//    val entryPoint = ast.getFunc(baseId)
-//    val result = ArrayList<CodegenFunc>(ast.funcTotalNodes - baseId + 1)
-//    result.add(CodegenFunc(-1, entryPoint.bodyId))
-//
-//    ast.funcSeek(pr.indFirstFunction)
-//    for (i in pr.indFirstFunction until ast.funcTotalNodes) {
-//        val newFunc = CodegenFunc(i, ast.funcCurrChunk.nodes[ast.funcCurrInd + 3])
-//        result.add(newFunc)
-//        val parentId = ast.funcCurrChunk.nodes[ast.funcCurrInd + 1]
-//        if (parentId > -1) {
-//            result[parentId - baseId].childFunctions.add(newFunc)
-//        }
-//        ast.funcNext()
-//    }
-//    return result
-//}
 
 
 }
--- source/codegen/CodegenTypes.kt
+++ source/codegen/CodegenTypes.kt
@@ -5,4 +5,7 @@ import parser.FrameAST
 
 data class CodegenFunc(val nameId: Int, val bodyInd: Int)
 
-data class CodegenFrame(val eType: FrameAST, val nameId: Int, val arity: Int, var currNode: Int = 0, var sentinelNode: Int = 0)
+class CodegenFrame(val eType: FrameAST, val nameId: Int, val arity: Int, startNode: Int, val lenNodes: Int) {
+    var currNode: Int = startNode
+    val sentinelNode: Int = startNode + lenNodes
+}
